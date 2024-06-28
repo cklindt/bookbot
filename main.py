3f00b67013a2cc192c1a2c772f8752f06c1c183f
cklindt <cklindt2003@gmail.com>
2024-06-28 11:35:48 -0700

    Prints a comprehensive report on text

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def word_count(text):
     return len(text.split())
 
 def get_char_dict(text):
-    text.lower()
+    text = text.lower()
     chars = {}
     for char in text:
         if char in chars:
@@ -19,9 +19,31 @@ def get_char_dict(text):
         else:
             chars[char] = 1
     return chars
+
+def alpha_dict(dict):
+    alpha_dict = {}
+    for char in dict:
+        if char.isalpha(): 
+            alpha_dict[char] = dict[char]
+    return alpha_dict
+def list_dict(dict):
+    list_dict = []
+    for char in dict:
+        list_dict.append({"char": char, "num":dict[char]})
+    return list_dict
+
+def sort_on(dict):
+    return dict["num"]
+
 def print_report(text):
     print("--- Begin report of books/frankenstein.txt ---")
-    
     print(f"{word_count(text)} words found in the document")
+    chars = get_char_dict(text)
+    chars = alpha_dict(chars)
+    dict_list_chars = list_dict(chars)
+    dict_list_chars.sort(reverse=True, key=sort_on)
+    for dict in dict_list_chars:
+        print(f"The {dict['char']} character was found {dict['num']} times")
+    print("--- End report ---")
 
 main()
